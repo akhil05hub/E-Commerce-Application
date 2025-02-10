11dec9f2fd73eb19adefece35354c8e14849cb77
akhil05hub <hingeakhil6397@gmail.com>
2025-02-10 23:22:43 +0530

    Updates

--- src/Api/AxiosInstance1.jsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const AxiosInstance1 = axios.create({
-    baseURL: 'https://fakestoreapi.com/products',
-  });
-
-  export default AxiosInstance1;
--- src/Api/AxiosInstance2.jsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const AxiosInstance2 = axios.create({
-    baseURL: 'http://localhost:5000',
-  });
-
-export default AxiosInstance2
