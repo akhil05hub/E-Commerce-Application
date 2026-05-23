196ffffabdfaa0a84126d4494d702c4b569a8b5b
Hinge Akhil <hingeakhil6397@gmail.com>
2026-05-23 15:46:53 +0530

    Update Footer.jsx

--- src/Components/Pages/Footer.jsx
+++ src/Components/Pages/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   
   return (
     <div className=" w-[100vw] h-14 bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center items-center">
-    <p className="text-center text-xl text-white">Ⓒ Copyright 2024. Knight Bite Foods Pvt. Ltd. All Rights Reserved</p>
+    <p className="text-center text-xl text-white">Ⓒ Copyright 2026. Knight Bite Foods Pvt. Ltd. All Rights Reserved</p>
     </div>
   )
 }
